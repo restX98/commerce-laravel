33b0bb5ffba43007ad691f9fc140b74a76100347
Enrico <enrico.rest98@gmail.com>
2023-06-25 13:34:56 +0200

    Added translation

---
+++ resources/lang/it/validation.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'required' => 'Campo obbligatorio.',
+    'email' => 'Inserisci un indirizzo email valido.',
+    'custom' => [
+        'email.unique' => 'Questa mail è già stata registrata, effettua il login.',
+        'phoneNumber.unique' => 'Questo numero di telefono è già stata registrato, effettua il login.',
+        'password.confirmed' => 'Le password non corrispondono'
+    ],
+];
